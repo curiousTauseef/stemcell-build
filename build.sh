32cc84949b1c8f4aef78357e5db0b48b6c5d4683
s.matyukevich <s.matyukevich@gmail.com>
2016-05-26 08:21:11 +0000

    path updated

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo "Start build process"
-cd ../bosh-repo/
+cd bosh-repo/
 bundle install
 bundle exec rake stemcell:build_os_image[ubuntu,trusty,$PWD/tmp/ubuntu_base_image.tgz] 
 bundle exec rake stemcell:build_with_local_os_image[aws,xen,ubuntu,trusty,go,$PWD/tmp/ubuntu_base_image.tgz]
