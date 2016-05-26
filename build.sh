0e8e820fb2f51a99e26c7311ea452fa1da14eec7
s.matyukevich <s.matyukevich@gmail.com>
2016-05-26 08:07:16 +0000

    stmcell build added

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ echo "Start build process"
 cd ../bosh-repo/
 bundle install
 bundle exec rake stemcell:build_os_image[ubuntu,trusty,$PWD/tmp/ubuntu_base_image.tgz] 
+bundle exec rake stemcell:build_with_local_os_image[aws,xen,ubuntu,trusty,go,$PWD/tmp/ubuntu_base_image.tgz]
+
